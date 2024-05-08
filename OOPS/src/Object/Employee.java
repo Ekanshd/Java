022cfb00fce1444c9440f8527e3b73f79eb7ec66
Ekanshd <97661817+Ekanshd@users.noreply.github.com>
2024-05-08 10:24:01 +0530

    Add files via upload

---
+++ OOPS/src/Object/Employee.java
@@ -0,0 +1,17 @@
+package Object;
+
+public class Employee {
+
+	String pname="Jhon";
+	int age=15;
+	float weight=60.30f;
+	double height=6.3;
+	String color="fair";
+
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		System.out.println(e.pname+" "+e.age+" "+e.weight+""+e.height+" "+e.color);
+
+	}
+
+}
