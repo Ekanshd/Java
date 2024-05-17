9903f84d087017887662bfb3e947a1a88013cc61
Ekanshd <97661817+Ekanshd@users.noreply.github.com>
2024-05-17 09:50:58 +0530

    Add files via upload

---
+++ Days/may13/may16/TryCatchMethod.java
@@ -0,0 +1,20 @@
+package com.may16;
+
+
+public class TryCatchMethod {
+
+	public static void main(String[] args) {
+		try {
+
+		int c=10/0;
+		System.out.println(c);
+		}catch(Exception e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println("Hello");
+		
+	}
+
+}
